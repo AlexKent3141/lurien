d533bbb01131a0c9c60bfc5feb5dde74a93b5bcf
Alex Kent <alex.kent1@hotmail.com>
2021-02-26 11:09:13 +0000

    Added more complete example usage.

---
+++ example.cpp
@@ -0,0 +1,64 @@
+#include "lurien.h"
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <memory>
+
+const int TARGET = 1e8;
+
+void func2(int* count)
+{
+  LURIEN_SCOPE(func2)
+
+  for (int i = 0; i < TARGET; i++)
+  {
+    ++*count;
+  }
+}
+
+void func()
+{
+  LURIEN_SCOPE(outer)
+
+  int count = 0;
+
+  {
+    LURIEN_SCOPE(inner2)
+
+    for (int i = 0; i < TARGET / 2; i++)
+    {
+      ++count;
+    }
+
+    {
+      LURIEN_SCOPE(inner3)
+
+      for (int i = 0; i < TARGET / 10; i++)
+      {
+        ++count;
+      }
+    }
+  }
+
+  for (int i = 0; i < 2; i++)
+  {
+    func2(&count);
+  }
+}
+
+int main()
+{
+  LURIEN_INIT(std::make_unique<lurien::DefaultOutputReceiver>(std::cout));
+
+  std::thread t1(&func);
+  std::thread t2(&func);
+  std::thread t3(&func);
+
+  t1.join();
+  t2.join();
+  t3.join();
+
+  LURIEN_STOP
+
+  return 0;
+}
